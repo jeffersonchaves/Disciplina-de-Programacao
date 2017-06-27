9e3b673eb9f81402524a9928b062d0a23099bb8f
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2017-06-27 10:46:40 -0300

    Adicionando funcionalidades de cadastro

--- SEGUNDO TRIMESTRE/agenda/controlador_agenda.php
+++ SEGUNDO TRIMESTRE/agenda/controlador_agenda.php
@@ -1,1 +1,42 @@
-//controlador agenda
+<?php //controlador agenda
+
+    function cadastrar(){
+
+        //colar aqui pelo amor dos meus bigodes
+        $contatos = file_get_contents("contatos.json", true); //guardando os resultados
+        $contatos = json_decode($contatos, true); // convertendo para um array
+
+        $contato = [
+            "id"       => uniqid(), //gerar um id novo e diferente de todos cada vez que atualizar
+            "nome"     => $_POST['nome'],
+            "email"    => $_POST['email'],
+            "telefone" => $_POST['telefone']
+        ];
+
+        array_push($contatos, $contato);
+
+        $dados_json = json_encode($contatos, JSON_PRETTY_PRINT); //arrumar na hora de executar
+
+        //atualizar o conteudo do arquivo
+        file_put_contents("contatos.json", $dados_json);
+
+        header('Location: index.php');
+
+
+    } //fim cadastrar()
+
+
+    function pegarContatos(){
+
+        $contatos = file_get_contents("contatos.json", true); //guardando os resultados
+        $contatos = json_decode($contatos, true); // convertendo para um array
+
+        return $contatos;
+
+    }
+
+
+    //GERENCIAMENTE DE ROTAS
+    if ($_GET['acao'] == 'cadastrar'){
+        cadastrar();
+    }
